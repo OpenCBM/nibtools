6f9396ce0e32e89ee41cdde2b90e5b04edf1b46d
peter <peter@048eaad1-c872-4173-8c30-523bb29f53ec>
2010-01-27 03:08:56 +0000

    repaired error in sync add logic
    git-svn-id: https://c64preservation.com/svn/nibtools/trunk@269 048eaad1-c872-4173-8c30-523bb29f53ec

--- write.c
+++ write.c
@@ -38,7 +38,7 @@ master_track(CBM_FILE fd, BYTE *track_buffer, BYTE *track_density, int track, si
 	if( (track_density[track] & BM_NO_SYNC) ||
 	    (align_map[track/2] == ALIGN_AUTOGAP) ||
 		( (track_buffer[track * NIB_TRACK_LENGTH] == 0xff) &&
-	 	  (track_buffer[(track * NIB_TRACK_LENGTH) + 1] == 0xff) ) )
+	 	  (track_buffer[(track * NIB_TRACK_LENGTH) + 1] != 0xff) ) )
 	{
 			/* merge in our track data */
 			memcpy(rawtrack + LEADER + skewbytes,  track_buffer + (track * NIB_TRACK_LENGTH), track_length);
