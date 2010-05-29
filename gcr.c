0247ba515de7762a224fb7a9bcc401b7b23d5d5a
peter <peter@048eaad1-c872-4173-8c30-523bb29f53ec>
2010-05-29 16:59:40 +0000

    further fixes/optimization for cycle detection
    git-svn-id: https://c64preservation.com/svn/nibtools/trunk@297 048eaad1-c872-4173-8c30-523bb29f53ec

--- gcr.c
+++ gcr.c
@@ -539,8 +539,7 @@ find_track_cycle(BYTE ** cycle_start, BYTE ** cycle_stop, size_t cap_min, size_t
 	for (p1 = start_pos; p1 < stop_pos - gap_match_length; p1+= gap_match_length)
 	{
 		/* now try to match it */
-		//for (p2 = stop_pos - gap_match_length; p2 > start_pos; p2--)
-		for (p2 = p1 + cap_max - gap_match_length; p2 > p1 + cap_min; p2--)
+		for (p2 = start_pos + cap_max; p2 > p1 + cap_min; p2--)
 		{
 			/* try to match data */
 			if (memcmp(p1, p2, gap_match_length) != 0)
