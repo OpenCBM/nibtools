8f2dab8fa9e80277c99f4a0fbd0342611f374cd5
peter <peter@048eaad1-c872-4173-8c30-523bb29f53ec>
2010-08-11 01:36:17 +0000

    try to fix ancient Crc code for x64
    git-svn-id: https://c64preservation.com/svn/nibtools/trunk@398 048eaad1-c872-4173-8c30-523bb29f53ec

--- crc.c
+++ crc.c
@@ -1,20 +1,20 @@
 /**********************************************************************
  *
  * Filename:    crc.c
- * 
+ *
  * Description: Slow and fast implementations of the CRC standards.
  *
  * Notes:       The parameters for each supported CRC standard are
  *				defined in the header file crc.h.  The implementations
  *				here should stand up to further additions to that list.
  *
- * 
+ *
  * Copyright (c) 2000 by Michael Barr.  This software is placed into
  * the public domain and may be used for any purpose.  However, this
  * notice must not be changed or removed and no warranty is either
  * expressed or implied by its publication or distribution.
  **********************************************************************/
- 
+
 #include "crc.h"
 
 
@@ -44,7 +44,7 @@
 /*********************************************************************
  *
  * Function:    reflect()
- * 
+ *
  * Description: Reorder the bits of a binary sequence, by reflecting
  *				them about the middle position.
  *
@@ -83,10 +83,10 @@ reflect(unsigned long data, unsigned char nBits)
 /*********************************************************************
  *
  * Function:    crcSlow()
- * 
+ *
  * Description: Compute the CRC of a given message.
  *
- * Notes:		
+ * Notes:
  *
  * Returns:		The CRC of the message.
  *
@@ -95,7 +95,7 @@ crc
 crcSlow(unsigned char const message[], int nBytes)
 {
     crc            remainder = INITIAL_REMAINDER;
-	int            byte;
+	unsigned char byte;
 	unsigned char  bit;
 
 
@@ -142,7 +142,7 @@ crc  crcTable[256];
 /*********************************************************************
  *
  * Function:    crcInit()
- * 
+ *
  * Description: Populate the partial CRC lookup table.
  *
  * Notes:		This function must be rerun any time the CRC standard
@@ -177,7 +177,7 @@ crcInit(void)
         {
             /*
              * Try to divide the current data bit.
-             */			
+             */
             if (remainder & TOPBIT)
             {
                 remainder = (remainder << 1) ^ POLYNOMIAL;
@@ -200,7 +200,7 @@ crcInit(void)
 /*********************************************************************
  *
  * Function:    crcFast()
- * 
+ *
  * Description: Compute the CRC of a given message.
  *
  * Notes:		crcInit() must be called first.
