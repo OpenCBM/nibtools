ee22695c438cc51714544ba22d935293930875b7
peter <peter@048eaad1-c872-4173-8c30-523bb29f53ec>
2012-05-13 22:56:58 +0000

    fixed issue with halftrack images where the last track of data was missing from the output
    git-svn-id: https://c64preservation.com/svn/nibtools/trunk@563 048eaad1-c872-4173-8c30-523bb29f53ec

--- fileio.c
+++ fileio.c
@@ -1024,7 +1024,7 @@ int write_g64(char *filename, BYTE *track_buffer, BYTE *track_density, size_t *t
 	}
 
 	/* shuffle raw GCR between formats */
-	for (track = 2; track <= MAX_HALFTRACKS_1541; track += track_inc)
+	for (track = 2; track <= MAX_HALFTRACKS_1541+1; track += track_inc)
 	{
 		size_t raw_track_size[4] = { 6250, 6666, 7142, 7692 };
 
