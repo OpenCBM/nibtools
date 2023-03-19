01c70701141622cf044c91d93c3ded3edc545a41
peter <peter@048eaad1-c872-4173-8c30-523bb29f53ec>
2023-03-19 16:52:09 +0000

    fat track detection was too weak
    git-svn-id: https://c64preservation.com/svn/nibtools/trunk@697 048eaad1-c872-4173-8c30-523bb29f53ec

--- prot.c
+++ prot.c
@@ -34,7 +34,7 @@ void search_fat_tracks(BYTE *track_buffer, BYTE *track_density, size_t *track_le
 
 				if(verbose>1) printf("%4.1f: %d\n",(float)track/2,diff);
 
-				if (diff<2) /* 34 happens on empty formatted disks */
+				if (diff<=33) /* 34 happens on empty formatted disks */
 				{
 					printf("Likely fat track found on T%d/%d (diff=%d)\n",track/2,(track/2)+1,(int)diff);
 
